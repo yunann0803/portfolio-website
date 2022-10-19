3522d1dc2aac67740a5d4374d4d552dc322fbea7
yunann0803 <jessica429285@gmail.com>
2022-10-19 13:49:40 +0800

    title change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ class MyDocument extends Document {
                 <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         <meta 
-        name="description" content="Urban Planner"/>
+        name="description" content="Consultant in Infrastructure Advisory"/>
         <meta 
         name="keywords" content="Urban Planner, Economist, Heritage Advocate "/>
 
